2811a64dadf6f5f98c39d59ab6a889f2bec11a80
Hemanta Kar <mr.hemanta@hotmail.com>
2023-06-24 13:13:06 +0530

    Frontend update 24-6-23

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
-  return <div> Welcome to E-Shop </div>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import Logo from "../assets/eShop-favicon.png";
+//import Logo from "../assets/E-shop-logo-1.png";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="light" variant="light" expand="lg" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img src={Logo} alt="logo" className="eshop-logo" />{" "}
+              <strong>E-SHOP</strong>
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart />
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser />
+                  Sign In
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
